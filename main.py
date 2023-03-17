1632efe6c53ce03fc4e3eee250df97a6475de3e4
juliorc03-student <julian.1701910633@ucaldas.edu.co>
2023-03-17 16:04:43 -0500

    Agregación de metodo getAllUsers, getUser, y postUser

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    UserName: str
+    Password: str
 
 @app.get("/")
 def read_root():
@@ -34,6 +39,14 @@ def view_one_user_id(id_user: str):
             return i
     return {"Message": "User does'nt exist"}
         
+@app.post("/user/")
+async def create_user(user: User):
+    for usuario in usuarios:
+        if user.UserName == usuario["UserName"]:
+            return {"Message": "User already exists"}
+    
+    usuarios.append(user)
+    return usuarios
 
 '''@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
